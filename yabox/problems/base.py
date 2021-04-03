0554268baa166396e9b71e469a9169fd7a160d70
Yamen Mubarka <mubarka1@llnl.gov>
2021-04-02 19:04:59 -0700

    added log plotting

--- yabox/problems/base.py
+++ yabox/problems/base.py
@@ -42,43 +42,56 @@ class BaseProblem:
         raise NotImplementedError('Implement the evaluation function')
 
     def plot2d(self, points=100, figure=None, figsize=(12, 8), contour=True, contour_levels=20,
-               imshow_kwds=None, contour_kwds=None):
+               imshow_kwds=None, contour_kwds=None, plot_log=False):
         if imshow_kwds is None:
             imshow_kwds = dict(cmap=cm.PuRd_r)
         if contour_kwds is None:
             contour_kwds = dict(cmap=cm.PuRd_r)
+
         xbounds, ybounds = self.bounds[0], self.bounds[1]
         x = np.linspace(min(xbounds), max(xbounds), points)
         y = np.linspace(min(xbounds), max(xbounds), points)
         X, Y = np.meshgrid(x, y)
         Z = self(np.asarray([X, Y]))
+        if plot_log:
+            Z = Z - np.min(Z) + 1
+            Z = np.log(Z)
+
         if figure is None:
             fig = plt.figure(figsize=figsize)
         else:
             fig = figure
+
         ax = fig.gca()
+
         if contour:
-            ax.contourf(X, Y, Z, contour_levels, **contour_kwds)
+            im = ax.contourf(X, Y, Z, contour_levels, **contour_kwds)
         else:
             im = ax.imshow(Z, **imshow_kwds)
-        if figure is None:
-            plt.show()
-        return fig, ax
+
+        return fig, ax, im
 
     def plot3d(self, points=100, contour_levels=20, ax3d=None, figsize=(12, 8),
-               view_init=None, surface_kwds=None, contour_kwds=None):
+               view_init=None, surface_kwds=None, contour_kwds=None, plot_log=False):
         from mpl_toolkits.mplot3d import Axes3D
+
         contour_settings = dict(contourParams)
         surface_settings = dict(surfaceParams)
+
         if contour_kwds is not None:
             contour_settings.update(contour_kwds)
         if surface_kwds is not None:
             surface_settings.update(surface_kwds)
+
         xbounds, ybounds = self.bounds[0], self.bounds[1]
         x = np.linspace(min(xbounds), max(xbounds), points)
         y = np.linspace(min(ybounds), max(ybounds), points)
         X, Y = np.meshgrid(x, y)
         Z = self(np.asarray([X, Y]))
+        if plot_log:
+            Z = Z - np.min(Z) + 1
+            Z = np.log(Z)
+
         if ax3d is None:
             fig = plt.figure(figsize=figsize)
             ax = Axes3D(fig)
@@ -86,6 +99,7 @@ class BaseProblem:
                 ax.view_init(*view_init)
         else:
             ax = ax3d
+
         # Make the background transparent
         ax.patch.set_alpha(0.0)
         # Make each axis pane transparent as well
@@ -95,9 +109,8 @@ class BaseProblem:
         surf = ax.plot_surface(X, Y, Z, **surface_settings)
         contour_settings['offset'] = np.min(Z)
         cont = ax.contourf(X, Y, Z, contour_levels, **contour_settings)
-        if ax3d is None:
-            plt.show()
-        return ax
+
+        return fig, ax, cont
 
     def __repr__(self):
         return '{} {}D'.format(self.name, self.dimensions)
